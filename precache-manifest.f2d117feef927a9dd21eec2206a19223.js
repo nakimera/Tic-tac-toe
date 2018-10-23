88e614b121ba676585fe363193d03af320bd9312
Nakimera Prossy <nakimeraprossy@Nakimeras-MacBook-Pro.local>
2018-10-23 08:59:16 +0300

    Updates

---
+++ precache-manifest.f2d117feef927a9dd21eec2206a19223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d07cf1557698cc505ee0",
+    "url": "/nakimera/Tic-tac-toe/static/js/runtime~main.d07cf155.js"
+  },
+  {
+    "revision": "f95d397574f2cc72bce0",
+    "url": "/nakimera/Tic-tac-toe/static/js/main.f95d3975.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/nakimera/Tic-tac-toe/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "f95d397574f2cc72bce0",
+    "url": "/nakimera/Tic-tac-toe/static/css/main.c1b74fe1.chunk.css"
+  },
+  {
+    "revision": "ae03b3507d494361a642f95c5e87683e",
+    "url": "/nakimera/Tic-tac-toe/index.html"
+  }
+];
